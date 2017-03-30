cf4b3dccd7531c62af0d18aa444f116161e2f636
Zachary Murphy <ztmurphy21@gmail.com>
2017-03-30 17:21:36 -0400

    fourth assignment

---
+++ Stock Solution-Highest/Stock Project/Main Form.Designer.vb
@@ -0,0 +1,157 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.lblHighest = New System.Windows.Forms.Label()
+        Me.lstPrices = New System.Windows.Forms.ListBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.btnCalc = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.lblDays = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        CType(Me.PictureBox1,System.ComponentModel.ISupportInitialize).BeginInit
+        Me.SuspendLayout
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = true
+        Me.Label1.Location = New System.Drawing.Point(248, 24)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(100, 20)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Highest price:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = true
+        Me.Label2.Location = New System.Drawing.Point(139, 24)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(91, 20)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "&Stock prices:"
+        '
+        'lblHighest
+        '
+        Me.lblHighest.BackColor = System.Drawing.Color.FromArgb(CType(CType(255,Byte),Integer), CType(CType(255,Byte),Integer), CType(CType(192,Byte),Integer))
+        Me.lblHighest.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblHighest.Font = New System.Drawing.Font("Segoe UI", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0,Byte))
+        Me.lblHighest.Location = New System.Drawing.Point(248, 47)
+        Me.lblHighest.Name = "lblHighest"
+        Me.lblHighest.Size = New System.Drawing.Size(154, 48)
+        Me.lblHighest.TabIndex = 5
+        Me.lblHighest.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lstPrices
+        '
+        Me.lstPrices.FormattingEnabled = true
+        Me.lstPrices.ItemHeight = 20
+        Me.lstPrices.Location = New System.Drawing.Point(143, 47)
+        Me.lstPrices.Name = "lstPrices"
+        Me.lstPrices.Size = New System.Drawing.Size(65, 204)
+        Me.lstPrices.TabIndex = 3
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.Stock_Project.My.Resources.Resources.Stocks
+        Me.PictureBox1.Location = New System.Drawing.Point(23, 24)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(102, 227)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 5
+        Me.PictureBox1.TabStop = false
+        '
+        'btnCalc
+        '
+        Me.btnCalc.Location = New System.Drawing.Point(248, 217)
+        Me.btnCalc.Name = "btnCalc"
+        Me.btnCalc.Size = New System.Drawing.Size(84, 34)
+        Me.btnCalc.TabIndex = 0
+        Me.btnCalc.Text = "&Calculate"
+        Me.btnCalc.UseVisualStyleBackColor = true
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(338, 217)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(64, 34)
+        Me.btnExit.TabIndex = 1
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = true
+        '
+        'lblDays
+        '
+        Me.lblDays.BackColor = System.Drawing.Color.FromArgb(CType(CType(255,Byte),Integer), CType(CType(255,Byte),Integer), CType(CType(192,Byte),Integer))
+        Me.lblDays.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblDays.Font = New System.Drawing.Font("Segoe UI", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0,Byte))
+        Me.lblDays.Location = New System.Drawing.Point(248, 135)
+        Me.lblDays.Name = "lblDays"
+        Me.lblDays.Size = New System.Drawing.Size(154, 48)
+        Me.lblDays.TabIndex = 7
+        Me.lblDays.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = true
+        Me.Label4.Location = New System.Drawing.Point(248, 112)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(118, 20)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Number of days:"
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8!, 20!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(425, 273)
+        Me.Controls.Add(Me.lblDays)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnCalc)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.lstPrices)
+        Me.Controls.Add(Me.lblHighest)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Font = New System.Drawing.Font("Segoe UI", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0,Byte))
+        Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.Name = "frmMain"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Waterson Company"
+        CType(Me.PictureBox1,System.ComponentModel.ISupportInitialize).EndInit
+        Me.ResumeLayout(false)
+        Me.PerformLayout
+
+End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents lblHighest As Label
+    Friend WithEvents lstPrices As ListBox
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents btnCalc As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents lblDays As Label
+    Friend WithEvents Label4 As Label
+End Class
---
+++ Stock Solution-Highest/Stock Project/Main Form.vb
@@ -0,0 +1,49 @@
+﻿' Name:         Stock Project
+' Purpose:      Displays the highest stock price and number of days at that price
+' Programmer:   Zach Murphy on 3-30-2017
+
+Option Explicit On
+Option Strict On
+Option Infer Off
+
+Public Class frmMain
+
+    ' class-level array
+    Private dblPrices() As Double = {91.8, 91.6, 92,
+        90, 90.5, 90.05, 90.12, 90.7, 90.8, 90.83, 92, 89.5, 90, 92}
+
+    Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles Me.Load
+        ' fill list box with prices
+
+        For Each dblElement As Double In dblPrices
+            lstPrices.Items.Add(dblElement.ToString("n2"))
+        Next dblElement
+    End Sub
+
+    Private Sub btnCalc_Click(sender As Object, e As EventArgs) Handles btnCalc.Click
+        ' displays the highest stock price and number of
+        ' days the stock closed at that price
+
+        Dim intLastSub As Integer = dblPrices.GetUpperBound(0)
+        Dim dblHighest As Double = dblPrices(0)
+        Dim intDays As Integer = 1
+
+        For intSub As Integer = 1 To intLastSub
+            If dblPrices(intSub) = dblHighest Then
+                intDays += 1
+            Else
+                If dblPrices(intSub) > dblHighest Then
+                    dblHighest = dblPrices(intSub)
+                    intDays = 1
+                End If
+            End If
+        Next intSub
+
+        lblHighest.Text = dblHighest.ToString("c2")
+        lblDays.Text = intDays.ToString
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+End Class
